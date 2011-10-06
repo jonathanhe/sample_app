4980e054bb31fb2be0e97ba200092871c52452e1
Jonathan He <jonathanhe@gmail.com>
2011-10-06 04:32:47 -0400

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Added by Jonathan on 10/6/11 to return a title
+  # for each page
+  def title
+    base_title = "aabb"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
