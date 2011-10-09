31de4e6c69f676282199a27961bfb2967078bdd3
Jonathan He <jonathanhe@gmail.com>
2011-10-09 22:20:18 +0800

    Create basic user model, added validation and test to ensure both name and email are nonempty

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  # Jonathan: use attr_accessible to prevent from 
+  # mass assignment vulnerability
+  attr_accessible :name, :email
+
+  validates :name,  :presence => true
+  validates :email, :presence => true
 end
