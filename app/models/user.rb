3f922aaf7ed2bad92cd27477c0a14d6c7129d91c
Jonathan He <jonathanhe@gmail.com>
2011-10-09 22:25:55 +0800

    Validate the length of name should be no longer than 50

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # mass assignment vulnerability
   attr_accessible :name, :email
 
-  validates :name,  :presence => true
+  validates :name,  :presence => true,
+                    :length => { :maximum => 50 }
   validates :email, :presence => true
 end
