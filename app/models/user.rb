4cb7ed29afdf3d922ff491f3c1ba22271e8715fb
Jonathan He <jonathanhe@gmail.com>
2011-10-10 00:38:18 -0400

    Start validating and testing email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
   # mass assignment vulnerability
   attr_accessible :name, :email
 
+  # validate email address
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name,  :presence => true,
                     :length => { :maximum => 50 }
-  validates :email, :presence => true
+  validates :email, :presence => true,
+                    :format => { :with => email_regex }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,4 +26,20 @@ describe User do
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
+
+  it "should accept valid email address" do
+    addresses = %w[user@example.com THE_USER@foo.bar.org first.last@bar.cn]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email address" do
+    addresses = %w[user@example,com user_at_foo.org example.user@bar]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
 end
