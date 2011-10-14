666f6d3497f5fdbf7ba5f9a69291645ee8a20471
Jonathan He <jonathanhe@gmail.com>
2011-10-14 02:01:57 -0400

    Test title and some content of the show html page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,7 +14,8 @@ describe UsersController do
 
     it "should have the right title" do
       get 'new'
-      response.should have_selector('title:contains("Sign up")')
+      #response.should have_selector('title:contains("Sign up")')
+      response.should have_selector("title", :content => "Sign up")
     end
   end
 
@@ -33,6 +34,16 @@ describe UsersController do
     it "should find the right user" do
       get :show, :id => @user
       assigns(:user).should == @user
+    end
+
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name in h1" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
     end
   end
 
