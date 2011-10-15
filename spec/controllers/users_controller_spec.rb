42c0b96ba054fb7e35d8268ca396af35cab17d00
Jonathan He <jonathanhe@gmail.com>
2011-10-15 17:31:18 +0800

    Add sign up form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,13 +8,15 @@ describe UsersController do
   describe "GET 'new'" do
 
     it "should be successful" do
-      get 'new'
+      #get 'new'
+      get :new
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'new'
+      #get 'new'
       #response.should have_selector('title:contains("Sign up")')
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
   end
