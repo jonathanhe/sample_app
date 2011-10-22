ff2d171976bc318ffb7397b2347fc02aa7200074
Jonathan He <jonathanhe@gmail.com>
2011-10-22 22:29:13 +0800

    Add successfully test cases for microposts creation

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -15,4 +15,50 @@ describe MicropostsController do
       response.should redirect_to signin_path
     end
   end
+
+  describe "POST 'create'" do
+   
+    before(:each) do
+      test_sign_in(Factory(:user))
+    end
+
+    describe "failure" do
+      before(:each) do
+        @attr = { :content =>"" }
+      end
+
+      it "should not create a micropost" do
+        lambda do
+          post :create, :micropost => @attr
+        end.should_not change(Micropost, :count)
+      end
+
+      it "should render the homepage" do
+        post :create, :micropost => @attr
+        response.should render_template('pages/home')
+      end
+    end
+
+    describe "success" do
+      before(:each) do
+        @attr = { :content => "Sample post" }
+      end
+
+      it "should create a micropost" do
+        lambda do
+          post :create, :micropost => @attr
+        end.should change(Micropost, :count).by(1)
+      end
+
+      it "should redirect user to root_path" do
+        post :create, :micropost => @attr
+        response.should redirect_to root_path
+      end
+
+      it "should have a success flash message" do
+        post :create, :micropost => @attr
+        flash[:success].should =~ /success/i
+      end
+    end
+  end
 end
