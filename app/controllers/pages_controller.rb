693d9238b531acc460bf253673742f9f6b033846
Jonathan He <jonathanhe@gmail.com>
2011-10-23 17:39:00 +0800

    Create the Micropost object at pages/home function, which fixed the broken tests. Also modified the stylesheets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    # It is important to create Micropost object here
+    # since we are going to show the users' microposts
+    # on the home page.
+    @micropost = Micropost.new if signed_in?
   end
 
   def contact
