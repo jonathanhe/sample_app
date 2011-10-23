162436f4695c9fd3218c042b9bcb9e6e7e5fb15a
Jonathan He <jonathanhe@gmail.com>
2011-10-23 21:40:58 +0800

    Implement destroy function for micropost

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -61,4 +61,43 @@ describe MicropostsController do
       end
     end
   end
+
+  describe "DELETE 'destroy'" do
+
+    describe "for an unthorized user" do
+
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        test_sign_in(wrong_user)
+        @micropost = Factory(:micropost, :user => @user)
+      end
+
+      it "should deny access" do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+
+      it "should not change count" do
+        lambda do
+          delete :destroy, :id => @micropost
+        end.should_not change(Micropost, :count)
+      end
+    end
+
+    describe "for an authorized user" do
+
+      before(:each) do
+        @user = Factory(:user)
+        test_sign_in(@user)
+        @mp = Factory(:micropost, :user => @user)
+      end
+
+      it "should destroy the micropost" do
+        lambda do
+          delete :destroy, :id => @mp
+        end.should change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
