33d420e8e910ec77f521290c6feb97b6ac6b9a07
Jonathan He <jonathanhe@gmail.com>
2011-10-24 08:31:18 +0800

    Add a success integration test case for micropost

--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -19,4 +19,15 @@ describe "Microposts" do
       response.should have_selector("div.field_with_errors")
     end.should_not change(Micropost, :count)
   end
+
+  it "should create a micropost with valid content" do
+    content = "sample content"
+    lambda do
+      visit root_path
+      fill_in :micropost_content, :with => content
+      click_button
+      flash[:success].should =~ /successfully saved/i
+      response.should have_selector("span.content", :content => content)
+    end.should change(Micropost, :count).by(1)
+  end
 end
