73c7399d6d3651261fae8114ee664420ff65a304
Jonathan He <jonathanhe@gmail.com>
2011-10-28 07:06:36 +0800

    further optimize the followed_ids SQL query using subselect

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,8 +9,17 @@ class Micropost < ActiveRecord::Base
   validates :content, :presence => true,
                       :length => { :maximum => 140 }
 
-  def self.from_users_followed_by(user)
-    followed_ids = user.following.map(&:id).join(", ")
-    where("user_id IN (#{followed_ids}) or user_id = ?", user)
-  end
+  default_scope :order => 'microposts.created_at DESC'
+
+  # return microposts from the users being followed by a given user
+  scope :from_users_followed_by, lambda { |user| followed_by(user) }
+
+  private
+
+    def self.followed_by(user)
+      followed_ids = %(SELECT followed_id FROM relationships
+                       WHERE follower_id = :user_id)
+      where("user_id IN (#{followed_ids}) OR user_id = :user_id",
+            { :user_id => user })
+    end
 end
